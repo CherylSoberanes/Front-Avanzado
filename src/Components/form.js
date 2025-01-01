10de6dfee0abcc56095d505e29b0561943f753fd
CherylSoberanes <cherylsoberanes@outlook.es>
2025-01-01 15:15:49 -0700

    Se añadio la verificacion del email, el tipo de fecha, el menu item de País, el tipo edad

--- src/Components/form.js
+++ src/Components/form.js
@@ -1,5 +1,5 @@
 import React from "react"; 
-import { Box, Grid2, TextField, Typography, Button } from "@mui/material";
+import { Box, Grid2, TextField, Typography, Button, MenuItem } from "@mui/material";
 import useForm from "../hooks/useForm";
 
 const Form = () => {
@@ -13,12 +13,17 @@ const Form = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+       
+    if (!/\S+@\S+.\S+/.test(formData.email)){
+
+        }
+   
         console.log(formData);
         resetForm();
     };
 
     return (
-        <Box sx={{maxWidth: 400, margin: 'auto', mt: 5}}>
+        <Box sx={{maxWidth: 470, margin: 'auto', mt: 5}}>
             <Typography variant="h4" align="center" gutterBottom>
                 Formulario en React Avanzado
             </Typography>
@@ -36,40 +41,52 @@ const Form = () => {
                     </Grid2>
                     <Grid2 item xs={12}>
                         <TextField
-                        label="Correo"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        fullWidth
-                        required
+                            label="Correo"
+                            name="email"
+                            type="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            fullWidth
+                            required
                         />
                     </Grid2>
                     <Grid2 item xs={12}>
                         <TextField
-                        label="Fecha"
-                        name="date"
-                        value={formData.date}
-                        onChange={handleChange}
-                        fullWidth
-                        required
+                            label="Fecha"
+                            name="date"
+                            type="date"
+                            value={formData.date}
+                            onChange={handleChange}
+                            fullWidth
+                            InputLabelProps={{shrink: true}}
+                            required
                         />
                     </Grid2>
                     <Grid2 item xs={12}>
                         <TextField
-                        label="País"
-                        name="country"
-                        value={formData.country}
-                        fullWidth
-                        required
-                        />
+                            select
+                            label="País"
+                            name="country"
+                            value={formData.country}
+                            onChange={handleChange}
+                            fullWidth
+                            required
+                            >
+                            <MenuItem value="Mexico">México</MenuItem>
+                            <MenuItem value="Usa">USA</MenuItem>
+                            <MenuItem value="Canada">Canadá</MenuItem>
+                            <MenuItem value="Argentina">Argentina</MenuItem>
+                        </TextField>
                     </Grid2>
                     <Grid2 item xs={12}>
                         <TextField
-                        label="Edad"
-                        name="age"
-                        value={formData.age}
-                        fullWidth
-                        required
+                            label="Edad"
+                            name="age"
+                            type="number"
+                            value={formData.age}
+                            onChange={handleChange}
+                            fullWidth
+                            required
                         />
                     </Grid2>
                     <Grid2 item xs={12}>
@@ -88,6 +105,7 @@ const Form = () => {
 
         </Box>
     );
+
 
 };
 
