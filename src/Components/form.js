00ee9766c0c35b09d051febdc8d14a2e9743b3e5
CherylSoberanes <cherylsoberanes@outlook.es>
2024-12-31 21:25:37 -0700

    Agregamos el Boton submit del formulario

--- src/Components/form.js
+++ src/Components/form.js
@@ -1,5 +1,5 @@
 import React from "react"; 
-import { Box, Grid2, TextField, Typography } from "@mui/material";
+import { Box, Grid2, TextField, Typography, Button } from "@mui/material";
 import useForm from "../hooks/useForm";
 
 const Form = () => {
@@ -18,26 +18,65 @@ const Form = () => {
             </Typography>
             <form onSubmit={handleSubmit}>
                 <Grid2 container spacing={2}>
-                    <TextField
-                    label="Nombre"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    fullWidth
-                    required
-                    />
+                    <Grid2 item xs={12}>
+                        <TextField
+                        label="Nombre"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        fullWidth
+                        required
+                        />
+                    </Grid2>
+                    <Grid2 item xs={12}>
+                        <TextField
+                        label="Correo"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        fullWidth
+                        required
+                        />
+                    </Grid2>
+                    <Grid2 item xs={12}>
+                        <TextField
+                        label="Fecha"
+                        name="date"
+                        value={formData.date}
+                        onChange={handleChange}
+                        fullWidth
+                        required
+                        />
+                    </Grid2>
+                    <Grid2 item xs={12}>
+                        <TextField
+                        label="País"
+                        name="country"
+                        value={formData.country}
+                        fullWidth
+                        required
+                        />
+                    </Grid2>
+                    <Grid2 item xs={12}>
+                        <TextField
+                        label="Edad"
+                        name="age"
+                        value={formData.age}
+                        fullWidth
+                        required
+                        />
+                    </Grid2>
+                    <Grid2 item xs={12}>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            fullWidth
+                            sx={{mt: 2}}
+                        />
+                    </Grid2>
+                
                 </Grid2>
-                <Grid2 item xs={12}>
-                    <TextField
-                    label="Correo"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    fullWidth
-                    required
-                    />
-                </Grid2>
-              
 
             </form>
 
